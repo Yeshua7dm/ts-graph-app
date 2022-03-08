efe4e2759928de0e5634f6b84a893e81bb97f083
Yeshua7dm <yeshua7dm@gmail.com>
2022-03-08 11:41:33 +0100

    configured config

---
+++ src/Config.ts
@@ -0,0 +1,14 @@
+const config = {
+    appId: '5a33477f-ecbb-49d6-9157-0931c5f2174c',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read',
+        'mailboxsettings.read',
+        'mail.read',
+        'mail.readwrite', 
+        'mail.readbasic',
+        'calendars.readwrite'
+    ]
+};
+
+export default config;
